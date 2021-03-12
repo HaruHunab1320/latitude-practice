22484b90400be74ebbfb5e01fef3537d423cb081
Haru <51176775+HaruHunab1320@users.noreply.github.com>
2021-03-11 20:54:14 -0800

    Build out Board Component

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,3 +1,11 @@
-const Board = () => {};
+import Square from "./Square";
+
+const Board = ({ squares, onClick }) => (
+  <div>
+    {squares.map((square, i) => (
+      <Square value={square} onClick={onClick(i)} />
+    ))}
+  </div>
+);
 
 export default Board;

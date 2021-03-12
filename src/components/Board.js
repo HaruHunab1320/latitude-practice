601fc7a5bb66b35d86b2db8e48cdad6ac129d23f
Haru <51176775+HaruHunab1320@users.noreply.github.com>
2021-03-11 20:54:46 -0800

    Add style to Board

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,7 +1,15 @@
 import Square from "./Square";
 
+const style = {
+  display: "grid",
+  gridTemplateColumn: "repeat(3, 1fr)",
+  boarder: "solid 4x black",
+  height: "300px",
+  width: "300px"
+};
+
 const Board = ({ squares, onClick }) => (
-  <div>
+  <div style={style}>
     {squares.map((square, i) => (
       <Square value={square} onClick={onClick(i)} />
     ))}

6c145c52eea02a69bb93fca9a49dc57cc1e0c5d2
Haru <51176775+HaruHunab1320@users.noreply.github.com>
2021-03-11 20:33:22 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Jakobs TicTacToe</h1>
+      <h2>Please Hire Me</h2>
+    </div>
+  );
+}
